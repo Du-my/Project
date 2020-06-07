41e7cf8772a5966f428fd1a9910a477d8fa6397f
dumen <dumenghao615@163.com>
2020-06-07 22:41:44 +0800

    first

---
+++ test/hello.py
@@ -0,0 +1,37 @@
+# print('hello python')
+#定义函数
+# def test():
+#     print('step1')
+#     print('step2')
+#test()
+# def test(a,b):
+#     return a,b, a+b
+# res = test(1,2)
+#
+# print(res)
+
+
+
+# def test(a,b):
+#     if a > b:
+#         return 1
+#     elif a < b:
+#         return -1
+#     else:
+#         return 0
+# # if test(10,20)!=0:
+# #     print('不相等')
+# res = test(5,6)
+# print(res)
+
+# alist = [10,20]
+# print(alist)
+# print(str(alist),'len----->',(str(alist)))
+
+#int() 转成int类型
+# str = '100.5'
+# print(int(str))
+
+#小数（浮点数）只取整数
+# print(int(3.14))
+# print(round(3.1415926,2))
